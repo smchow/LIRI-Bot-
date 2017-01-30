6d35828064caea54c027e51efeabfeca923a4959
smchow <sunita.m.chowdhury@gmail.com>
2017-01-30 13:39:50 -0500

    added RT ratings

--- liri.js
+++ liri.js
@@ -7,6 +7,8 @@ var fs = require("fs");
 
 var action = "";
 var actStr = '';//yellow submarine';
+
+
 fs.readFile("./random.txt", "utf8", function(err, data){
 
 	console.log(data);
@@ -16,28 +18,10 @@ fs.readFile("./random.txt", "utf8", function(err, data){
 
 console.log(actStr);
 
-	for (var i=0; i < myArr.length; i++){
-		console.log(myArr[i]);
 
-	}
-
-  spotify.search({ type: 'track', query: actStr }, function(err, data) {
-    if ( err ) {
-        console.log('Error occurred: ' + err);
-        return;
-    }
- 
-    /*Artist(s)
-    The song's name
-    A preview link of the song from Spotify
-    The album that the song is from*/
-    for (var i =0 ; i < data.tracks.items.length; i++){
-      console.log(data.tracks.items[i].name + " " 
-          + data.tracks.items[0].album.name + " " + data.tracks.items[0].preview_url 
-          + " " + data.tracks.items[0].artists[0].name);
-    }
-    
-});
+  console.log(searchSong(''));
+  var myMovie = getmovieInfo('');
+  console.log(myMovie);
 });
 
 var client = new Twitter({
@@ -56,12 +40,31 @@ client.get('statuses/user_timeline', params, function(error, tweets, response) {
     }
   }
 
-  console.log(getmovieInfo(''));
+  
 });
 
 
 
-
+var searchSong = function(songStr){
+    spotify.search({ type: 'track', query: actStr }, function(err, data) {
+    if ( err ) {
+        console.log('Error occurred: ' + err);
+        return;
+    }
+ 
+    /*Artist(s)
+    The song's name
+    A preview link of the song from Spotify
+    The album that the song is from*/
+    //for (var i =0 ; i < data.tracks.items.length; i++){
+      var songStr = data.tracks.items[0].name + " " 
+          + data.tracks.items[0].album.name + " " + data.tracks.items[0].preview_url 
+          + " " + data.tracks.items[0].artists[0].name ;
+        return songStr;
+    //}
+    
+});
+}
 
 /*Title of the movie.
 Year the movie came out.
@@ -74,7 +77,7 @@ Rotten Tomatoes Rating.
 Rotten Tomatoes URL.*/
 
 
- var getmovieInfo = function(movieStr){ request("http://www.omdbapi.com/?t=remember+the+titans&y=&plot=short&r=json", function(error, response, body) {
+ var getmovieInfo = function(movieStr){ request("http://www.omdbapi.com/?t=remember+the+titans&y=&plot=short&r=json&tomatoes=true", function(error, response, body) {
        var movieInfo = '';
      // If the request is successful (i.e. if the response status code is 200)
      if (!error && response.statusCode === 200) {
@@ -82,15 +85,15 @@ Rotten Tomatoes URL.*/
        // Parse the body of the site and recover just the imdbRating
        // (Note: The syntax below for parsing isn't obvious. Just spend a few moments dissecting it).
          movieInfo += "\nThe movie's title is: " + JSON.parse(body).Title;
-         movieInfo += "\nThe movie's year is: " + JSON.parse(body).Year;
+         movieInfo += "\nThe movie's was made in: " + JSON.parse(body).Year;
          movieInfo += "\nThe movie's rating is: " + JSON.parse(body).imdbRating;
-         movieInfo += "The movie's country is: " + JSON.parse(body).Country;
-         movieInfo += "The movie's language is: " + JSON.parse(body).Language;
-         movieInfo += "The movie's plot is: " + JSON.parse(body).Plot;
-         movieInfo += "The movie's actors is: " + JSON.parse(body).Actors;
-         movieInfo += "The movie's rating is: " + JSON.parse(body).imdbRating;
-         movieInfo += "The movie's rating is: " + JSON.parse(body).imdbRating;
- 
+         movieInfo += "\nThe movie's country is: " + JSON.parse(body).Country;
+         movieInfo += "\nThe movie's language is: " + JSON.parse(body).Language;
+         movieInfo += "\nThe movie's plot is: " + JSON.parse(body).Plot;
+         movieInfo += "\nThe movie's actors is: " + JSON.parse(body).Actors;
+         movieInfo += "\nThe movie's rotten tomatoes rating is: " + JSON.parse(body).tomatoRating;
+         movieInfo += "\nThe movie's rotten tomatoes url is: " + JSON.parse(body).tomatoURL;
+         console.log(movieInfo)
          return movieInfo;
      }
  });
