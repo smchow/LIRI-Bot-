f50ad38e8c474b060723ee1ce725bdff9ea6b0b9
smchow <sunita.m.chowdhury@gmail.com>
2017-01-30 12:39:32 -0500

    refactoring code

--- liri.js
+++ liri.js
@@ -2,6 +2,7 @@ var Twitter = require('twitter');
 var apiKey = require('./keys.js');
 var spotify = require('spotify');
 var request = require("request");
+var inquirer = require("inquirer");
 var fs = require("fs");
 
 var action = "";
@@ -55,7 +56,7 @@ client.get('statuses/user_timeline', params, function(error, tweets, response) {
     }
   }
 
-  getmovieInfo('');
+  console.log(getmovieInfo(''));
 });
 
 
@@ -72,27 +73,6 @@ Actors in the movie.
 Rotten Tomatoes Rating.
 Rotten Tomatoes URL.*/
 
-/*"Title": "Remember the Titans",
-"Year": "2000",
-"Rated": "PG",
-"Released": "29 Sep 2000",
-"Runtime": "113 min",
-"Genre": "Biography, Drama, Sport",
-"Director": "Boaz Yakin",
-"Writer": "Gregory Allen Howard",
-"Actors": "Denzel Washington, Will Patton, Wood Harris, Ryan Hurst",
-"Plot": "The true story of a newly appointed African-American coach and his high school team on their first season as a racially integrated unit.",
-"Language": "English",
-"Country": "USA",
-"Awards": "8 wins & 17 nominations.",
-"Poster": "https://images-na.ssl-images-amazon.com/images/M/MV5BYThkMzgxNjEtMzFiOC00MTI0LWI5MDItNDVmYjA4NzY5MDQ2L2ltYWdlL2ltYWdlXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_SX300.jpg",
-"Metascore": "48",
-"imdbRating": "7.8",
-"imdbVotes": "165,591",
-"imdbID": "tt0210945",
-"Type": "movie",
-"Response": "True"*/
-//}
 
  var getmovieInfo = function(movieStr){ request("http://www.omdbapi.com/?t=remember+the+titans&y=&plot=short&r=json", function(error, response, body) {
        var movieInfo = '';
@@ -101,9 +81,9 @@ Rotten Tomatoes URL.*/
  
        // Parse the body of the site and recover just the imdbRating
        // (Note: The syntax below for parsing isn't obvious. Just spend a few moments dissecting it).
-         movieInfo += "The movie's title is: " + JSON.parse(body).Title;
-         movieInfo += "The movie's year is: " + JSON.parse(body).Year;
-         movieInfo += "The movie's rating is: " + JSON.parse(body).imdbRating;
+         movieInfo += "\nThe movie's title is: " + JSON.parse(body).Title;
+         movieInfo += "\nThe movie's year is: " + JSON.parse(body).Year;
+         movieInfo += "\nThe movie's rating is: " + JSON.parse(body).imdbRating;
          movieInfo += "The movie's country is: " + JSON.parse(body).Country;
          movieInfo += "The movie's language is: " + JSON.parse(body).Language;
          movieInfo += "The movie's plot is: " + JSON.parse(body).Plot;
@@ -111,7 +91,7 @@ Rotten Tomatoes URL.*/
          movieInfo += "The movie's rating is: " + JSON.parse(body).imdbRating;
          movieInfo += "The movie's rating is: " + JSON.parse(body).imdbRating;
  
-         console.log(movieInfo);
+         return movieInfo;
      }
  });
 }
