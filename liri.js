e77d8ce08d449bde3585950eb2e48fabfad228ef
smchow <sunita.m.chowdhury@gmail.com>
2017-01-30 11:25:18 -0500

    read random.txt to get default arguments

--- liri.js
+++ liri.js
@@ -2,39 +2,25 @@ var Twitter = require('twitter');
 var apiKey = require('./keys.js');
 var spotify = require('spotify');
 var request = require("request");
+var fs = require("fs");
 
-
-/* var fs = require("fs");
-
-fs.readFile("./keys.js", "utf8", function(err, data){
+var action = "";
+var actStr = '';//yellow submarine';
+fs.readFile("./random.txt", "utf8", function(err, data){
 
 	console.log(data);
 	var myArr = data.split(",");
-	
+  action = myArr[0];
+	actStr = myArr[1];
+
+console.log(actStr);
+
 	for (var i=0; i < myArr.length; i++){
 		console.log(myArr[i]);
+
 	}
-});
-*/
-var client = new Twitter({
-  consumer_key: apiKey.twitterKeys.consumer_key,
-  consumer_secret: apiKey.twitterKeys.consumer_secret,
-  access_token_key: apiKey.twitterKeys.access_token_key,
-  access_token_secret: apiKey.twitterKeys.access_token_secret
-});
 
-var params = {screen_name: 'coderSun'};
-client.get('statuses/user_timeline', params, function(error, tweets, response) {
-  if (!error) {
-    console.log(tweets.length);
-    for (var i = 0 ; i < tweets.length; i++ ){
-      console.log(tweets[i].text + " " + tweets[i].created_at);
-    }
-  }
-});
-
-
-spotify.search({ type: 'track', query: 'yellow submarine' }, function(err, data) {
+  spotify.search({ type: 'track', query: actStr }, function(err, data) {
     if ( err ) {
         console.log('Error occurred: ' + err);
         return;
@@ -51,6 +37,29 @@ spotify.search({ type: 'track', query: 'yellow submarine' }, function(err, data)
     }
     
 });
+});
+
+var client = new Twitter({
+  consumer_key: apiKey.twitterKeys.consumer_key,
+  consumer_secret: apiKey.twitterKeys.consumer_secret,
+  access_token_key: apiKey.twitterKeys.access_token_key,
+  access_token_secret: apiKey.twitterKeys.access_token_secret
+});
+
+var params = {screen_name: 'coderSun'};
+client.get('statuses/user_timeline', params, function(error, tweets, response) {
+  if (!error) {
+    console.log(tweets.length);
+    for (var i = 0 ; i < tweets.length; i++ ){
+      console.log(tweets[i].text + " " + tweets[i].created_at);
+    }
+  }
+
+  getmovieInfo('');
+});
+
+
+
 
 
 /*Title of the movie.
@@ -85,23 +94,24 @@ Rotten Tomatoes URL.*/
 "Response": "True"*/
 //}
 
-request("http://www.omdbapi.com/?t=remember+the+titans&y=&plot=short&r=json", function(error, response, body) {
-    var movieInfo = '';
-  // If the request is successful (i.e. if the response status code is 200)
-  if (!error && response.statusCode === 200) {
-
-    // Parse the body of the site and recover just the imdbRating
-    // (Note: The syntax below for parsing isn't obvious. Just spend a few moments dissecting it).
-      movieInfo += "The movie's title is: " + JSON.parse(body).Title;
-      movieInfo += "The movie's year is: " + JSON.parse(body).Year;
-      movieInfo += "The movie's rating is: " + JSON.parse(body).imdbRating;
-      movieInfo += "The movie's country is: " + JSON.parse(body).Country;
-      movieInfo += "The movie's language is: " + JSON.parse(body).Language;
-      movieInfo += "The movie's plot is: " + JSON.parse(body).Plot;
-      movieInfo += "The movie's actors is: " + JSON.parse(body).Actors;
-      movieInfo += "The movie's rating is: " + JSON.parse(body).imdbRating;
-      movieInfo += "The movie's rating is: " + JSON.parse(body).imdbRating;
-
-      console.log(movieInfo);
-  }
-});
+ var getmovieInfo = function(movieStr){ request("http://www.omdbapi.com/?t=remember+the+titans&y=&plot=short&r=json", function(error, response, body) {
+       var movieInfo = '';
+     // If the request is successful (i.e. if the response status code is 200)
+     if (!error && response.statusCode === 200) {
+ 
+       // Parse the body of the site and recover just the imdbRating
+       // (Note: The syntax below for parsing isn't obvious. Just spend a few moments dissecting it).
+         movieInfo += "The movie's title is: " + JSON.parse(body).Title;
+         movieInfo += "The movie's year is: " + JSON.parse(body).Year;
+         movieInfo += "The movie's rating is: " + JSON.parse(body).imdbRating;
+         movieInfo += "The movie's country is: " + JSON.parse(body).Country;
+         movieInfo += "The movie's language is: " + JSON.parse(body).Language;
+         movieInfo += "The movie's plot is: " + JSON.parse(body).Plot;
+         movieInfo += "The movie's actors is: " + JSON.parse(body).Actors;
+         movieInfo += "The movie's rating is: " + JSON.parse(body).imdbRating;
+         movieInfo += "The movie's rating is: " + JSON.parse(body).imdbRating;
+ 
+         console.log(movieInfo);
+     }
+ });
+}
