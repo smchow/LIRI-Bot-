5f6db4e4d16057968bdaa92365db4326d108a2a3
smchow <sunita.m.chowdhury@gmail.com>
2017-01-31 12:18:46 -0500

    add code to accept user input'

--- liri.js
+++ liri.js
@@ -5,34 +5,7 @@ var request = require("request");
 var inquirer = require("inquirer");
 var fs = require("fs");
 
-var action = "";
-var actStr = '';//yellow submarine';
 
-
-
-
-
-
-var searchSong = function(songStr){
-    spotify.search({ type: 'track', query: actStr }, function(err, data) {
-    if ( err ) {
-        console.log('Error occurred: ' + err);
-        return;
-    }
- 
-    /*Artist(s)
-    The song's name
-    A preview link of the song from Spotify
-    The album that the song is from*/
-    //for (var i =0 ; i < data.tracks.items.length; i++){
-      var songStr = data.tracks.items[0].name + " " 
-          + data.tracks.items[0].album.name + " " + data.tracks.items[0].preview_url 
-          + " " + data.tracks.items[0].artists[0].name ;
-        return songStr;
-    //}
-    
-});
-}
 
 /*Title of the movie.
 Year the movie came out.
@@ -45,8 +18,9 @@ Rotten Tomatoes Rating.
 Rotten Tomatoes URL.*/
 
 
- var getmovieInfo = function(movieStr){ request("http://www.omdbapi.com/?t=remember+the+titans&y=&plot=short&r=json&tomatoes=true", function(error, response, body) {
+ var getmovieInfo = function(movieStr){ request("http://www.omdbapi.com/?t="+ movieStr+"&y=&plot=short&r=json&tomatoes=true", function(error, response, body) {
        var movieInfo = '';
+       console.log(movieStr);
      // If the request is successful (i.e. if the response status code is 200)
      if (!error && response.statusCode === 200) {
  
@@ -67,27 +41,51 @@ Rotten Tomatoes URL.*/
  });
 }
 
+var searchSong = function(actStr){
+    console.log('in search song' + actStr + "  ");
+
+    spotify.search({ type: 'track', query: actStr }, function(err, data) {
+    if ( err ) {
+        console.log('Error occurred: ' + err);
+        return;
+    }
+ 
+    /*Artist(s)
+    The song's name
+    A preview link of the song from Spotify
+    The album that the song is from*/
+    //for (var i =0 ; i < data.tracks.items.length; i++){
+      var songStr = data.tracks.items[0].name + " " 
+          + data.tracks.items[0].album.name + " " + data.tracks.items[0].preview_url 
+          + " " + data.tracks.items[0].artists[0].name ;
+          console.log(songStr)
+        return songStr;
+    //}
+    
+});
+}
+
 var inquirer = require("inquirer");
 
 // Create a "Prompt" with a series of questions.
 inquirer.prompt([
+   {
+    type: "list",
+    message: "Which option do you choose?",
+    choices: ["my-tweets", "spotify-this-song", "movie-this", "do-what-it-says"],
+    name: "liriCmd"
+  },
 
   // Here we create a basic text prompt.
   {
     type: "input",
-    message: "What is your name?",
+    message: "Please enter the name of the song/movie (if applicable)?",
     name: "name"
-  },
+  }
 
 
   // Here we give the user a list to choose from.
-  {
-    type: "list",
-    message: "Which option do you choose?",
-    choices: ["my-tweets", "spotify-this-song", "movie-this", "do-what-it-says"],
-    name: "pokemon"
-  }
-
+ 
   // Here we ask the user to confirm.
 
 
@@ -95,27 +93,40 @@ inquirer.prompt([
 // In this case, we store all of the answers into a "user" object that inquirer makes for us.
 ]).then(function(user) {
   console.log(user);
+  var action = "";
+  var actStr = '';//yellow submarine';
 
   // If we log that user as a JSON, we can see how it looks.
   console.log(JSON.stringify(user, null, 2));
+  action = user.liriCmd;
+    actStr = user.name;
+  if (user.liriCmd == "do-what-it-says"){
+    fs.readFile("./random.txt", "utf8", function(err, data) {
 
+     console.log(data);
+     var myArr = data.split(",");
+     action = myArr[0];
+     actStr = myArr[1];
+     console.log(actStr);
+    });
+  }
+  //["", , ""
+  console.log(actStr +  "++actStr **" + action);
   // If the user confirms, we displays the user's name and pokemon from the answers.
-  fs.readFile("./random.txt", "utf8", function(err, data){
+  if (action == "spotify-this-song"){
 
-  console.log(data);
-  var myArr = data.split(",");
-  action = myArr[0];
-  actStr = myArr[1];
+      console.log(searchSong(actStr));
+  }
 
-console.log(actStr);
+  if (action == "movie-this"){
+     var myMovie = getmovieInfo('');
+      console.log(myMovie);
+  }
 
+ 
 
-  console.log(searchSong(''));
-  var myMovie = getmovieInfo('');
-  console.log(myMovie);
-});
-
-var client = new Twitter({
+if (action == "my-tweets"){
+  var client = new Twitter({
   consumer_key: apiKey.twitterKeys.consumer_key,
   consumer_secret: apiKey.twitterKeys.consumer_secret,
   access_token_key: apiKey.twitterKeys.access_token_key,
@@ -128,10 +139,12 @@ client.get('statuses/user_timeline', params, function(error, tweets, response) {
     console.log(tweets.length);
     for (var i = 0 ; i < tweets.length; i++ ){
       console.log(tweets[i].text + " " + tweets[i].created_at);
+      fs.appendFile('./log.txt', tweets[i].text + " " + tweets[i].created_at + "\n");
     }
   }
 
   
 });
+}
 
 });
